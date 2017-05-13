2e04e4139997b02ed1790251986c95b29230a4f1
mice530 <chenzhihao@126.com>
2017-05-13 10:40:35 +0800

    replace chai with assert

--- test/index.js
+++ test/index.js
@@ -1,43 +1,43 @@
 const ipv4toInt32 = require('../index');
-const assert = require('chai').assert;
+const assert = require('assert');
 
 describe('ipv4toInt32', () => {
   it('should handle regular input correctly', () => {
-    assert.equal( ipv4toInt32('172.168.5.1'), 2896692481 );
+    assert.strictEqual( ipv4toInt32('172.168.5.1'), 2896692481, 'test case 172.168.5.1 failed' );
 
-    assert.equal( ipv4toInt32('0.0.0.0'), 0 );
+    assert.strictEqual( ipv4toInt32('0.0.0.0'), 0 );
 
-    assert.equal( ipv4toInt32('255.255.255.255'), 4294967295 );
+    assert.strictEqual( ipv4toInt32('255.255.255.255'), 4294967295, 'test case 172.168.5.1 failed' );
   });
 
   it('should handle spaces correctly', () => {
-    assert.equal( ipv4toInt32('172 .168.5.1'), 2896692481 );
+    assert.strictEqual( ipv4toInt32('172 .168.5.1'), 2896692481, 'test case 172.168.5.1 failed' );
 
-    assert.equal( ipv4toInt32('172. 168.5.1'), 2896692481 );
+    assert.strictEqual( ipv4toInt32('172. 168.5.1'), 2896692481, 'test case 172.168.5.1 failed' );
 
-    assert.equal( ipv4toInt32('172.168 . 5.1'), 2896692481 );
+    assert.strictEqual( ipv4toInt32('172.168 . 5.1'), 2896692481, 'test case 172.168.5.1 failed' );
   });
 
   it('should report error on invalid input', () => {
     const errMsg = 'invalid input';
 
     // unexpect space
-    assert.equal( ipv4toInt32('17 2.168.5.1'), errMsg );
+    assert.strictEqual( ipv4toInt32('17 2.168.5.1'), errMsg, 'test case 17 2.168.5.1 failed' );
 
     // unexpect char
-    assert.equal( ipv4toInt32('172.168.f5.1'), errMsg );
+    assert.strictEqual( ipv4toInt32('172.168.f5.1'), errMsg, 'test case 172.168.f5.1 failed' );
 
     // unexpect numbers
-    assert.equal( ipv4toInt32('172.1668.5.1'), errMsg );
+    assert.strictEqual( ipv4toInt32('172.1668.5.1'), errMsg, 'test case 172.1668.5.1 failed' );
 
     // unexpect dot
-    assert.equal( ipv4toInt32('172..1668.5.1'), errMsg );
+    assert.strictEqual( ipv4toInt32('172..1668.5.1'), errMsg, 'test case 172..1668.5.1 failed' );
 
     // incorrect format
-    assert.equal( ipv4toInt32('1.172.1668.5.1'), errMsg );
-    assert.equal( ipv4toInt32('168.5.1'), errMsg );
-    assert.equal( ipv4toInt32('.68.5.1'), errMsg );
-    assert.equal( ipv4toInt32('172.68.256.1'), errMsg );
+    assert.strictEqual( ipv4toInt32('1.172.1668.5.1'), errMsg, 'test case 1.172.1668.5.1 failed' );
+    assert.strictEqual( ipv4toInt32('168.5.1'), errMsg, 'test case 168.5.1 failed' );
+    assert.strictEqual( ipv4toInt32('.68.5.1'), errMsg, 'test case .68.5.1 failed' );
+    assert.strictEqual( ipv4toInt32('172.68.256.1'), errMsg, 'test case 172.68.256.1 failed' );
 
 
   });
